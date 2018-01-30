e6eb7da17a7732b749a936f0b6484152007a6f11
Antonia Cronin <antoniajcronin@gmail.com>
2018-01-30 14:54:25 +0000

    planes take off

--- jasmine/spec/featureSpec.js
+++ jasmine/spec/featureSpec.js
@@ -1,12 +1,25 @@
 describe('Airport', function() {
 
-  var airport = new Airport()
-  var plane = new Plane()
+  var airport;
+  var plane;
 
-  describe('land a plane', function(){
+  beforeEach(function(){
+    airport = new Airport();
+    plane = new Plane();
+  });
+
+  describe('instruct plane to land', function(){
     it('lands', function() {
       airport.land(plane)
       expect(airport._hangar).toContain(plane)
+    });
+  });
+
+  describe('instruct plane to take off', function(){
+    it('takes off', function() {
+      airport.land(plane)
+      airport.take_off(plane)
+      expect(airport._hangar).not.toContain(plane)
     });
   });
 });
--- jasmine/src/airport.js
+++ jasmine/src/airport.js
@@ -1,10 +1,17 @@
 'use strict';
 
 function Airport() {
-  this._hangar = []
+  this._hangar = [];
 };
 
 Airport.prototype.land = function(plane) {
-  this._hangar.push(plane)
-  return true;
+  this._hangar.push(plane);
+  console.log("land plane", plane);
+  console.log(this._hangar);
+};
+
+Airport.prototype.take_off = function(plane) {
+  this._hangar.pop(plane);
+  console.log("Take off plane", plane);
+  console.log(this._hangar);
 };
