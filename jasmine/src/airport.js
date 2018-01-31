9895a624e2e5128598579d8064cbef4dcf04d2a1
Anjana <anjana.lpn@gmail.com>
2018-01-31 14:11:15 +0000

    prevents landing when airport full

--- jasmine/src/airport.js
+++ jasmine/src/airport.js
@@ -2,13 +2,15 @@
 
 function Airport(weatherStation) {
   this._hangar = [];
-  this._weatherStation = weatherStation
-  this._capacity = 10
+  this._weatherStation = weatherStation;
+  this._CAPACITY = 10;
 };
 
 Airport.prototype.land = function(plane) {
   if (this._weatherStation.isStormy()) {
     throw new Error("Too stormy to land");
+  } else if (this._hangar.length >= 10) {
+    throw new Error("The airport is full");
   } else {
     this._hangar.push(plane);
   };
